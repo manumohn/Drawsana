66716b5a56c14091b09b49d02ccf6d764ae653fc
Manu Mohan <>
2020-01-02 16:28:18 +0530

    Add beziershape implementation Add selection for penshape

---
+++ Drawsana/Shapes/Implementations/BezierShape.swift
@@ -0,0 +1,88 @@
+//
+//  BezierShape.swift
+//  Drawsana
+//
+//  Created by Manu Mohan on 02/01/20.
+//
+
+import Foundation
+
+public class BezierShape: Shape,
+                ShapeWithStandardState,
+                ShapeWithBezierPath,
+                ShapeSelectable {
+    
+    public var boundingRect: CGRect {
+        return bezierPath.bounds.insetBy(dx: -strokeWidth/2.0, dy: -strokeWidth/2.0)
+    }
+    
+    private enum CodingKeys: String, CodingKey {
+        case id, strokeColor, fillColor, strokeWidth, type, transform
+    }
+    
+    public var id: String = UUID().uuidString
+    public var strokeColor: UIColor? = .black
+    public var fillColor: UIColor? = .clear
+    public var strokeWidth: CGFloat = 3
+    public static var type: String = "BezierShape"
+    public var bezierPath: UIBezierPath = UIBezierPath()
+    public var transform: ShapeTransform = .identity
+    
+    public init() {
+        
+    }
+    
+    public required init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        
+        let type = try values.decode(String.self, forKey: .type)
+        if type != BezierShape.type {
+            throw DrawsanaDecodingError.wrongShapeTypeError
+        }
+        transform = try values.decodeIfPresent(ShapeTransform.self, forKey: .transform) ?? .identity
+        
+        id = try values.decode(String.self, forKey: .id)
+        
+        strokeColor = try values.decodeColorIfPresent(forKey: .strokeColor)
+        fillColor = try values.decodeColorIfPresent(forKey: .fillColor)
+        
+        strokeWidth = try values.decode(CGFloat.self, forKey: .strokeWidth)
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(EllipseShape.type, forKey: .type)
+        try container.encode(id, forKey: .id)
+        try container.encode(strokeColor?.hexString, forKey: .strokeColor)
+        try container.encode(fillColor?.hexString, forKey: .fillColor)
+        try container.encode(strokeWidth, forKey: .strokeWidth)
+
+        if !transform.isIdentity {
+            try container.encode(transform, forKey: .transform)
+        }
+    }
+    
+    public func render(in context: CGContext) {
+        transform.begin(context: context)
+        if let fillColor = fillColor {
+            context.setFillColor(fillColor.cgColor)
+            context.addPath(bezierPath.cgPath)
+            context.fillPath()
+        }
+        
+        context.setLineWidth(strokeWidth)
+        
+        if let strokeColor = strokeColor {
+            context.setStrokeColor(strokeColor.cgColor)
+            context.setLineDash(phase: 0, lengths: [])
+            context.addPath(bezierPath.cgPath)
+            context.strokePath()
+        }
+        transform.end(context: context)
+    }
+    
+    public func hitTest(point: CGPoint) -> Bool {
+        return bezierPath.contains(point)
+    }
+    
+}
